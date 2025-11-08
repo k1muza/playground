347b1d170dfd66094bfe824fd2818a7a380dda1c
kelvin muza <k1muza@gmail.com>
2025-11-08 12:52:02 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5327331875-6a3b2",
+  "appId": "1:241156852423:web:e519a7e850448e0188cce2",
+  "apiKey": "AIzaSyCIQTm5sPq3-lO7mZvWsssCFYuhq1JpwhA",
+  "authDomain": "studio-5327331875-6a3b2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "241156852423"
+};
